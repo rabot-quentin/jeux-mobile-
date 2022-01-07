cc0a18b750a446938b426b95f30568afecd95f17
Thomas L <seth.drakor@gmail.com>
2022-01-07 17:02:10 +0100

    CommitAll

--- Jeu Mobil/Assets/code/LevelLoader.cs
+++ Jeu Mobil/Assets/code/LevelLoader.cs
@@ -8,47 +8,6 @@ public class LevelLoader : MonoBehaviour
 {
     public GameObject background;
     
-    public Animator transition;
-
-    public bool buttonRestartPressed = false;
-    public bool buttonMenuPressed = false;
-
-    public float transitionTime = 1f;
-
-    void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
-        {
-            LoadLevel();
-        }
-    }
-
-    public void LoadLevel()
-    {
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
-    }
-
-    IEnumerator LoadLevel(int levelIndex)
-    {
-        background.SetActive(true);
-
-        transition.SetTrigger("Start");
-
-        yield return new WaitForSeconds(transitionTime);
-
-        SceneManager.LoadScene(levelIndex);
-
-        if (buttonRestartPressed = true)
-        {
-            OnButtonRestart();
-        }
-
-        if (buttonMenuPressed = true)
-        {
-            OnButtonMenu();
-        }
-    }
-
     public void LoadRestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
